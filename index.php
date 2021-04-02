fecec0abc94f344254628b719973bbb43fe3277f
francisco-pinto <ffcpinto2@gmail.com>
2021-04-02 15:24:46 +0100

    First Commit

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 ?>
 
     
-
+   
 <html lang = "en">
    
    <head>
--- teste.php
+++ teste.php
@@ -21,7 +21,7 @@
    // error_reporting(E_ALL);
    // ini_set("display_errors", 1);
 ?>
-    
+      
 
 
 <html lang = "en">
