3029a8565ad7fe1163fb550e4459b16b8f3c91e2
al68603@utad.eu <al68603@utad.eu>
2021-04-02 14:56:55 +0100

    First Commit

---
+++ teste.php
@@ -0,0 +1,159 @@
+<!--    Conexão à Base de Dados     -->
+<?php
+    $servername = "localhost";
+    $username = "root";         //Default credencials wamp
+    $password = "";
+
+    try {
+        $db = new PDO("mysql:host=$servername;dbname=Classificador_Codigo", $username, $password);
+        // set the PDO error mode to exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+
+   ob_start();
+   session_start();
+?>
+
+<?
+   // error_reporting(E_ALL);
+   // ini_set("display_errors", 1);
+?>
+
+
+
+<html lang = "en">
+   
+   <head>
+      <title>Login Form</title>
+      <!--<link href = "css/bootstrap.min.css" rel = "stylesheet">-->
+      
+      <style>
+         body {
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #ADABAB;
+         }
+         
+         .form-signin {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+            color: #017572;
+         }
+         
+         .form-signin .form-signin-heading,
+         .form-signin .checkbox {
+            margin-bottom: 10px;
+         }
+         
+         .form-signin .checkbox {
+            font-weight: normal;
+         }
+         
+         .form-signin .form-control {
+            position: relative;
+            height: auto;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            padding: 10px;
+            font-size: 16px;
+         }
+         
+         .form-signin .form-control:focus {
+            z-index: 2;
+         }
+         
+         .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+            border-color:#017572;
+         }
+         
+         .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+            border-color:#017572;
+         }
+         
+         h2{
+            text-align: center;
+            color: #017572;
+         }
+      </style>
+      
+   </head>
+	
+   <body>
+      
+      <h2>Enter Username and Password</h2> 
+      <div class = "container form-signin">
+    
+        <?php
+            $msg = '';
+            
+
+
+            if (isset($_POST['login']) && !empty($_POST['username']) 
+            && !empty($_POST['password'])) {
+                $username = trim($_POST['username']);
+                $password = trim($_POST['password']);
+                
+                try {
+                    $query = "select * from utilizador where Username=:username and Pass=:password";
+                    $stmt = $db->prepare($query);
+                    $stmt->bindParam('username', $username, PDO::PARAM_STR);
+                    $stmt->bindValue('password', $password, PDO::PARAM_STR);
+                    $stmt->execute();
+                    $count = $stmt->rowCount();
+                    $row   = $stmt->fetch(PDO::FETCH_ASSOC);
+                    
+                    if($count == 1 && !empty($row)) {
+                    /******************** Your code ***********************/
+                        $_SESSION['user_Id']   = $row['Id'];
+                        $_SESSION['user_Username'] = $row['Username'];
+                        $_SESSION['user_Name'] = $row['Nome'];
+
+                        $msg = "Login efetuado com sucesso";
+                        header("Location: index.php");
+                        die();
+                        
+                    } else {
+                        $msg = "Invalid username and password!";
+                    }
+                } catch (PDOException $e) {
+                    echo "Error : ".$e->getMessage();
+                }
+            } else {
+              $msg = "Both fields are required!";
+            }
+         ?>
+
+      </div> <!-- /container -->
+      
+      <div class = "container">
+      
+         <form class = "form-signin" role = "form" 
+            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
+            ?>" method = "post">
+            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            <input type = "text" class = "form-control" 
+               name = "username" placeholder = "Username" 
+               required autofocus></br>
+            <input type = "password" class = "form-control"
+               name = "password" placeholder = "Password" required>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
+               name = "login">Login</button>
+         </form>
+			
+         Click here to  <a href = "logout.php" tite = "Logout">Logout.
+         
+      </div> 
+      
+   </body>
+</html>
