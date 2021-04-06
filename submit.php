1355da304aa0e765dd66273f161e8c0a2ed6fc08
francisco-pinto <ffcpinto2@gmail.com>
2021-04-06 12:20:42 +0100

    Adição de dados à Database

--- submit.php
+++ submit.php
@@ -1,5 +1,26 @@
 <?php
+    ob_start();
+    session_start();
+
+    if (isset($_SESSION['user_Username']) && isset($_SESSION['user_Name'])) 
+    {
+        echo "Welcome to the member's area, " . $_SESSION['user_Username'] . "!";
+    }else 
+    {
+        header("Location: login.php");
+        die(); //pega
+    }
+
+
+
+
+/*Upload do ficheiro*/
+
     $file=$_FILES["fileToUpload"]["name"];
+    $fileSize=$_FILES["fileToUpload"]["size"];
+    $filePath=$_FILES["fileToUpload"]["tmp_name"];
+    $todayDate = date("Y-m-d H:i:s");
+
 
     $ext = pathinfo($file, PATHINFO_EXTENSION);
     //$allowedTypes = array('application/zip', 'application/x-rar-compressed');
@@ -34,5 +55,31 @@
             }
         }
     }   
+
+
+/*Introdução dos dados na BD*/
+    $servername = "localhost";
+    $username = "root";         //Default credencials wamp
+    $password = "";
+
+    try {
+        $db = new PDO("mysql:host=$servername;dbname=Classificador_Codigo", $username, $password);
+        // set the PDO error mode to exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+
+
+        $sql = "INSERT INTO ficheiro
+        VALUES (1, '$file', $fileSize, 'c', '$filePath', $todayDate, $_SESSION['user_Id'], 1);";
+        // use exec() because no results are returned
+        $db->exec($sql);
+        echo "Base de Dados atualizada";
+    
+    
+        $db = null;
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+
 
 ?>
