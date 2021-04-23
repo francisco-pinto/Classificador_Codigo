c76a9003ba2f561f4eccdb246e5c717ea0dd84ec
francisco-pinto <ffcpinto2@gmail.com>
2021-04-23 16:50:12 +0100

    Continuação da Página Projeto

--- projeto.php
+++ projeto.php
@@ -12,10 +12,25 @@
         header("Location: login.php");
         die(); //pega
     }
+
+
+    /*Ligação da Base de Dados*/
+    $servername = "localhost";
+    $username = "root";         //Default credencials wamp
+    $password = "";
+
+    try {
+        $db = new PDO("mysql:host=$servername;dbname=Classificador_Codigo", $username, $password);
+        // set the PDO error mode to exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
 ?>
 
          
-<html lang = "en">
+<html lang = "pt">
    
    <head>
       <title>Index</title>
@@ -76,28 +91,119 @@
             color: #017572;
          }
       </style>
-      
+
+        <!--De forma a conseguirmos obter um calendário-->
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script>
+            $( function() {
+                $( "#datepicker" ).datepicker();
+            } );
+        </script>
    </head>
 	
    <body>
       
-      <h2>Página Index</h2>  
-      
-      <?php
-        if(array_key_exists('button_logout', $_POST)) {
-            button1();
-        }
-        function button1() {
-            session_destroy();
-            header("Location: login.php");
-            die();
-        }
-    ?>
+        <h2>Criação de Projeto</h2>  
+        
+        <h3>Insira os critérios de avaliação</h3>
+        
 
-      <form method="post">
-        <input type="submit" name="button_logout"
-                class="button" value="Logout" />
-    </form>
-         
+        <!-- Quantos input e output iremos ter? Fazer dinamicamente casos de teste -->
+        <form action="" method="post" enctype="multipart/form-data">
+            <label for="name">Nome do Projeto:</label>
+            <input type="text" id="text" name="name"><br><br><br><br>
+            <label for="input">Input:</label>
+            <input type="text" id="input" name="input"><br><br>
+            <label for="output">Output:</label>
+            <input type="text" id="output" name="output"><br><br>
+            <br>
+            <br>
+            <br>
+            <br>  
+    
+        <!-- Escolha da linguagem de programação -->
+            <label for="Linguagens">Escolher a linguagem:</label><br>
+            <?php
+                $sql = "SELECT * FROM Linguagem";
+
+                $q = $db->prepare($sql);
+                $q->execute();
+                $q->setFetchMode(PDO::FETCH_ASSOC);
+
+                while ($linguagens = $q->fetch()) {
+                    echo '<input type="radio" id="' . $linguagens['Id'] . '" name="' . $linguagens['Id'] . '" value="' . $linguagens['Linguagem'] . '">';
+                    echo '<label for="' . $linguagens['Linguagem'] . '">' . $linguagens['Linguagem'] . '</label><br>';
+                }
+            ?>
+            <br>
+            <br>
+            <br>
+            <br>     
+
+
+            <!-- Data de início e data de fim do projeto -->
+            <p>Data de início de Projeto: <input type="text" id="datepicker" name="Begin_Date"></p>
+            <br><br>
+            <p>Data de Fim de Projeto: <input type="text" id="datepicker" name="End_Date"></p>
+
+            <input type="submit" value="Submeter" name="submit">
+        </form>
+
+
    </body>
 </html>
+
+
+<?php
+    if(isset($_POST['submit'])){
+        if(!empty($_POST['language'])){
+            if(!empty($_POST['name'])){
+                if(!empty($_POST['input']) && !empty($_POST['output'])){
+                    if(!empty($_POST['Begin_Date']) || $_POST['Begin_Date'] > date("Y/m/d")){
+
+                        echo "Início da colocação do projeto";
+
+                        
+                        $user_id =  $_SESSION['user_Id'];  
+                        $languageID = $_POST['language'];
+                        $name= $_POST['name'];
+                        $input = $_POST['input'];
+                        $output = $_POST['output'];
+                        $Begin_Date = $_POST['Begin_Date'];
+                        $End_Date = $_POST['End_Date'];
+
+
+
+
+                        $sql_Casos_Teste = "INSERT INTO Casos_Teste (Input, Output) VALUES ('$input', '$output');";
+                    
+                        $SQL_CasosTesteID = "SELECT ID FROM Casos_Teste WHERE Input = '$input' and Output = '$output'";
+                        $CasosTesteID = $db->exec($SQL_CasosTesteID);
+
+                        $sql = "INSERT INTO Projeto (Nome, Data_Projeto, Data_Limite, Linguagem, Data_Upload, CasosTesteID) VALUES ('$name', '$Begin_Date', '$End_Date', '$languageID', '$todayDate', '$CasosTesteID');";
+    
+                        // use exec() because no results are returned
+                        $db->exec($sql_Casos_Teste);
+                        $db->exec($sql);
+                        echo "<br><br><br>Base de Dados atualizada";
+                        die();
+                        
+
+                        echo "Base de Dados atualizada";
+                    }else{
+                        echo "Data Inválida";
+                    }
+                }else{
+                    echo "Complete os casos de teste";
+                }
+            }else{
+                echo "Insira o nome do projeto";
+            }
+        }else{
+            echo "Preencha os dados todos!";
+        }
+    }
+
+?>
